5670526ff25a4942b626b21412b835a05aa3fd98
sushidesu <hugemerge@gmail.com>
2020-06-09 03:37:35 +0000

    feat: update to calc sku

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react"
+import React, { useCallback, useRef } from "react"
+import { readSheet } from "./components/utils"
+import { COLUMNS } from "./constants"
 
 export default () => {
+  const fileInput = useRef(null)
+
+  const submit = useCallback(async () => {
+    const file = fileInput?.current?.files[0]
+    if (file) {
+      const sheet = await readSheet(file)
+      const sku = sheet
+        .filter(row => !isNaN(row[COLUMNS.STOCK]) && row[COLUMNS.STOCK] > 0)
+        .length
+      console.log(sku)
+    }
+
+  }, [])
+
   return (
     <div>
       <h1>sku-review</h1>
       <input
         type="file"
+        ref={fileInput}
       />
+      <button
+        onClick={submit}
+      >start</button>
     </div>
   )
 }
---
+++ src/constants.js
@@ -0,0 +1,10 @@
+
+export const COLUMNS = {
+  ITEM_NUMBER: 0,
+  JAN: 1,
+  NAME: 2,
+  PLACE: 3,
+  STOCK: 4,
+  WHOLESALE_PRICE: 5,
+  PRICE: 6,
+}
