f6b7ecb3730475a358112f209dd2b39d2524e813
sushidesu <hugemerge@gmail.com>
2020-06-09 08:23:53 +0000

    Add loading animation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,32 +3,53 @@ import { readSheet } from "./utils"
 import { COLUMNS } from "../constants"
 import { ReviewResult } from "./ReviewResult"
 import { FileInput } from "./FileInput"
+import { Loading } from "./Loading"
+
+type Status = "default" | "loading" | "done"
 
 export default () => {
+  const [status, setStatus] = useState<Status>("default")
   const [result, setResult] = useState<number | null>(null)
 
   const submit = useCallback(async (files: File[]) => {
     const file = files[0]
     if (file) {
+      setStatus("loading")
       const sheet = await readSheet(file)
       const sku = sheet
         .filter(row => !isNaN(row[COLUMNS.STOCK]) && row[COLUMNS.STOCK] > 0)
         .length
       setResult(sku)
+      setStatus("done")
     }
+  }, [setResult, setStatus])
 
-  }, [setResult])
+  const clear = useCallback(() => {
+    setStatus("default")
+    setResult(null)
+  }, [setStatus])
 
   return (
     <div>
       <h1>sku-review</h1>
 
-      <ReviewResult sku={result} />
+      {status === "done"
+        && <>
+          <ReviewResult sku={result} />
+          <button onClick={clear}>clear</button>
+        </>
+      }
 
-      <FileInput
-        accept=".xls"
-        onDrop={submit}
-      />
+      {status === "loading"
+        && <Loading /> 
+      }
+
+      {status === "default"
+        && <FileInput
+          accept=".xls"
+          onDrop={submit}
+        />
+      }
     </div>
   )
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { SyncLoader } from "react-spinners"
+
+export const Loading = () => (
+  <SyncLoader
+    color="#fab348"
+    size={20}
+    margin={4}
+  />
+)
