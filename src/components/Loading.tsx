187b3fcf75c0198cd5318c848ba6917f5e0108f7
sushidesu <hugemerge@gmail.com>
2020-06-12 02:09:38 +0000

    Fix Syncloader types

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,20 @@
-import React from "react"
+import React, { ComponentClass } from "react"
 import { SyncLoader } from "react-spinners"
 import { COLOR } from "../constants"
 
+type LengthType = number | string;
+
+type Props = {
+  color?: string
+  size?: LengthType
+  margin?: LengthType
+}
+type Override = ComponentClass<Props, any>
+
+const FixedSyncLoader = SyncLoader as Override
+
 export const Loading = () => (
-  <SyncLoader
+  <FixedSyncLoader
     color={COLOR.primary.default}
     size={20}
     margin={4}
